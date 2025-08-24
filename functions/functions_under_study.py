f54249c8f3147fbbd771303ad0448d734459e0ae
Roman Gorbunov <romangorbunov91@yandex.ru>
2025-08-24 23:17:26 +0700

    grad_method update
    Реализовано численное вычисление градиента для многомерных функций (опробовано на квадратичной функции, до 4-х независимых переменных).

--- functions/functions_under_study.py
+++ functions/functions_under_study.py
@@ -1,27 +1,36 @@
-# version 0.2 by romangorbunov91
-# 15-Aug-2025
+# version 0.1 by romangorbunov91
+# 24-Aug-2025
 
 import numpy as np
 
 # Functions.
-def f_well(x):
-    return 0.8 * x[0]**2 + 1.2 * x[1]**2 + 0.4 * x[0]*x[1]
+def f_2D(x):
+    return -0.5*x**3 - 3*x**2 - 5*x + 3
 
-def f_poor(x):
+def f_3D(x):
     return 2 * x[0]**2 + 205 * x[1]**2 - 10 * x[0]*x[1] + 20 * x[0] + 30 * x[1] + 23
 
-def f_rozen(x):
-    return (1-x[0])**2 + 100 * (x[1]-x[0]**2)**2
+def f_4D(x):
+    return 2 * x[0]**2 + 205 * x[1]**2 - 10 * x[0]*x[1] + 20 * x[0] + 30 * x[1] + 23 + 110 * x[2]**2
+
+def f_5D(x):
+    return 2 * x[0]**2 + 205 * x[1]**2 - 10 * x[0]*x[1] + 20 * x[0] + 30 * x[1] + 23 + 110 * x[2]**2 - 4 * x[3]**3
 
 # Gradients.
-def grad_well(x):
-    return np.array([1.6*x[0] + 0.4*x[1],
-                     0.4*x[0] + 2.4*x[1]])
+def grad_2D(x):
+    return -1.5*x**2 - 6*x - 5
 
-def grad_poor(x):
+def grad_3D(x):
     return np.array([4*x[0] - 10*x[1] + 20,
                      -10*x[0] + 410*x[1] + 30])
 
-def grad_rozen(x):
-    return np.array([400*x[0]**3 + 2*x[0] - 400*x[0]*x[1] - 2,
-                     -200*x[0]**2 + 200*x[1]])
+def grad_4D(x):
+    return np.array([4*x[0] - 10*x[1] + 20,
+                     -10*x[0] + 410*x[1] + 30,
+                     220*x[2]])
+
+def grad_5D(x):
+    return np.array([4*x[0] - 10*x[1] + 20,
+                     -10*x[0] + 410*x[1] + 30,
+                     220*x[2],
+                     -12*x[3]**2])
